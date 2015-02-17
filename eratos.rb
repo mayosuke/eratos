257cfe8f7a74c8d31b809b62d3adecbd7d7a5f26
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2015-02-18 00:59:01 +0900

    Implement Sieve of Eratosthenes.

--- eratos.rb
+++ eratos.rb
@@ -1,12 +1,24 @@
 # coding: utf-8
 
 class Eratos
-  def self.calc_eratos_for(number)
-    "2, 3, 5, 7, 11, 13, 17, 19, 23, 29"
+  def self.calc_eratos_for(x)
+    # 探索リストに2からxまでの整数を昇順で入れる
+    integers = (2..x).to_a
+    return integers if integers.length == 1
+    # 探索リストの先頭の数を素数リストに移動し、その倍数を探索リストから篩い落とす
+    primes = [integers.first]
+    integers = integers.select {|i| i.modulo(primes.last) != 0}
+    # 上記の篩い落とし操作を探索リストの先頭値がxの平方根に達するまで行う
+    while integers.first <= Math.sqrt(x)
+      primes << integers.shift
+      integers = integers.select {|i| i.modulo(primes.last) != 0}
+    end
+    # 探索リストに残った数を素数リストに移動して処理終了
+    primes += integers
   end
 end
 
 if __FILE__ == $0
-  print Eratos.calc_eratos_for(ARGV)
+  print Eratos.calc_eratos_for(Integer(ARGV[0])).join(', ')
 end
 
--- spec/eratos_spec.rb
+++ spec/eratos_spec.rb
@@ -9,6 +9,9 @@ describe Eratos do
   specify { expect(`ruby ./eratos.rb 5`).to eq "2, 3, 5"}
   specify { expect(`ruby ./eratos.rb 6`).to eq "2, 3, 5"}
   specify { expect(`ruby ./eratos.rb 7`).to eq "2, 3, 5, 7"}
+  specify { expect(`ruby ./eratos.rb 8`).to eq "2, 3, 5, 7"}
+  specify { expect(`ruby ./eratos.rb 9`).to eq "2, 3, 5, 7"}
+  specify { expect(`ruby ./eratos.rb 10`).to eq "2, 3, 5, 7"}
   specify { expect(`ruby ./eratos.rb 30`).to eq "2, 3, 5, 7, 11, 13, 17, 19, 23, 29"}
   specify { expect(`ruby ./eratos.rb 100`).to eq "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97"}
 end
